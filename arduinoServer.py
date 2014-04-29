6bf055a3a88451f611f6b6bcf5c275edd26f863f
Lorenzo <lorenzo.millucci@outlook.com>
2014-04-29 10:19:01 +0200

    ADD: metodi da implementare

--- arduinoServer.py
+++ arduinoServer.py
@@ -1,1 +1,29 @@
 #TO-DO
+
+class Arduino:
+  
+  	def changeSpeed(self,value1, value2 ):
+  	  pass
+
+    #motore 1
+    def motor1Orario(self):
+      pass
+
+    def motor1AntiOrario(self):
+      pass
+    
+    #motore 2
+    def motor2Orario(self):
+      pass
+    
+    def motor2AntiOrario(self):
+      pass
+    
+    def __init__(self):
+      pass
+    
+    def setMotor(self,u,e):
+      pass
+
+    def onClose(self):
+      pass
