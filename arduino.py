2a413d9c9a4399854ca789d8394b6651aa54d93d
Lorenzo <lorenzo.millucci@outlook.com>
2014-05-02 15:17:17 +0200

    UPDATE:riscritto il codice con la giusta indentazione

---
+++ arduino.py
@@ -0,0 +1,32 @@
+import time
+import serial
+
+class Arduino:
+	ser = ""
+	ENDING = "\n"
+	
+	def __init__(self):
+		self.ser = "Ciao"
+		#self.ser = serial.Serial('/dev/ttyACM0', 9600)
+		
+	def setMotor(self, u, e):
+		direction = ""
+		if e < -40:
+			#turn Left
+			print "Left"
+			direction = "l" + self.ENDING
+
+		elif e > 40:
+			#turn right
+			print "Right"
+			direction = "r" + self.ENDING
+
+		else:
+			#go Forward
+			print "Forward"
+			u=75
+			direction = "f" + self.ENDING
+		
+		direction += str(u)+ self.ENDING 
+		print direction
+		#ser.write(direction)
