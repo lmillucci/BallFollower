770788977b35870deb940a0675898cd0f29d8e00
lomby92 <giacomo.lombardi@live.it>
2014-08-01 12:36:35 +0200

    update server

--- CamServerTCP.py
+++ CamServerTCP.py
@@ -3,10 +3,10 @@ import socket, cv2, numpy
 TCP_IP = ''
 TCP_PORT = 32230
 
-
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
-server_socket.bind((TCP_IP,TCP_PORT)) 
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind((TCP_IP,TCP_PORT))
 server_socket.listen(5)
+
 capture = cv2.VideoCapture(0)
 capture.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, 640)
 capture.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, 480)
@@ -18,18 +18,11 @@ while True:
         client_socket, address = server_socket.accept()
         print "Open socket whit: " , address
     _,frame = capture.read()
-    '''print img
-    print "Tipo img: ",type(img)
-    print "Grandezza img",img.shape
-    print "Tipo di dato img: ",img.dtype
-    print "Numero di elementi: ",img.size'''
+    
     minColor = numpy.array((26,75,67))
     maxColor = numpy.array((256,256,256))
     frame2 = cv2.inRange(frame,minColor,maxColor)
-    '''print "Tipo img: ",type(img2)
-    print "Grandezza img",img2.shape
-    print "Tipo di dato img: ",img2.dtype
-    print "Numero di elementi: ",img2.size'''
+
     encode_param=[int(cv2.IMWRITE_JPEG_QUALITY),90]
     #encoding frame and frame2
     result, imgencoded = cv2.imencode('.jpg', frame, encode_param)
@@ -45,14 +38,27 @@ while True:
     except:
         pass
     try:
-        if client_socket.recv(1024)=="end":
+        ricevuto = client_socket.recv(1024)
+        try:
+            direzione = ricevuto.split("velocita")
+        except:
+            direzione = None
+            pass
+        if ricevuto=="end":
             print "Ho ricevuto l'end dal Client"
             client_socket.close()
             client_socket = None
+        elif(direzione[0]=="comando:F"):
+            print "Forward at ",direzione[1]
+        elif(direzione[0]=="comando:L"):
+            print "Left at ",direzione[1]
+        elif(direzione[0]=="comando:R"):
+            print "Right at ",direzione[1]
         else:
-            print "Ho ricevuto il continue dal Client"
+            #devo continuare
     except:
         client_socket.close()
         client_socket = None
+    
 cv2.VideoCapture(0).release()
 server_socket.close()
